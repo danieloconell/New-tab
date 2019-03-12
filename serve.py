8138c9442921c5a8327c140b47f821abe35c6124
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2019-03-12 11:26:17 +1100

    Ignore python cache

--- serve.py
+++ serve.py
@@ -1,25 +1,25 @@
-from flask import Flask, jsonify, Response
-from requests import get
-from ics import Calendar
+from flask import Flask, jsonify
+import requests
+import ics
 
-app = Flask(__name__)
 
 cal_url = "https://rosebank.instructure.com/feeds/calendars/user_VjkmU5ODxKymLFNVtq51pRHNU6zQ1rZYIG09JgRR.ics"
 
 
-@app.route("/")
-def index():
-    c = Calendar(get(cal_url).text)
-    response = jsonify(
-        {
-            index: [event.name, event.begin.humanize(), event.description]
-            for index, event in enumerate(c.events)
-        }
-    )
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    return response
+def create_app():
+    app = Flask(__name__)
 
+    @app.route("/")
+    def index():
+        c = ics.Calendar(requests.get(cal_url).text)
+        response = jsonify(
+            {
+                index: [event.name, event.begin.humanize(), event.description]
+                for index, event in enumerate(c.events)
+            }
+        )
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        return response
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    return app
